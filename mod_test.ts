d8ffa109143a0935c34a1282f065027752b38e90
Shuntaro Nishizawa <me@shun.technology>
2023-03-17 17:28:32 +0900

    test(mod_test.ts): add tests for renderToString function with different options and cases

--- mod_test.ts
+++ mod_test.ts
@@ -1,34 +1,62 @@
 import { assertEquals } from "./dev_deps.ts";
 import { element, renderToString } from "./mod.ts";
 
-Deno.test("render (basic)", () => {
-  const root = element("root").text("Hello World!");
-  const xml = renderToString(root, { version: "1.0" });
+Deno.test("renderToString", async (t) => {
+  await t.step("basic", () => {
+    const root = element("root").text("Hello World!");
+    const xml = renderToString(root);
 
-  assertEquals(
-    xml,
-    `<?xml version="1.0" encoding="UTF-8"?><root>Hello World!</root>`,
-  );
-});
+    assertEquals(
+      xml,
+      `<?xml version="1.0" encoding="UTF-8"?><root>Hello World!</root>`,
+    );
+  });
 
-Deno.test("render (attributes)", () => {
-  const root = element("root").attr("foo", "bar").text("Hello World!");
-  const xml = renderToString(root, { version: "1.0" });
+  await t.step("attributes", () => {
+    const root = element("root").attr("foo", "bar").text("Hello World!");
+    const xml = renderToString(root, { version: "1.0" });
 
-  assertEquals(
-    xml,
-    `<?xml version="1.0" encoding="UTF-8"?><root foo="bar">Hello World!</root>`,
-  );
-});
+    assertEquals(
+      xml,
+      `<?xml version="1.0" encoding="UTF-8"?><root foo="bar">Hello World!</root>`,
+    );
+  });
 
-Deno.test("render (children)", () => {
-  const root = element("root")
-    .child(element("child").text("Hello World!"))
-    .child(element("child").text("Goodbye World!"));
-  const xml = renderToString(root, { version: "1.0" });
+  await t.step("children", () => {
+    const root = element("root")
+      .child(element("child").text("Hello World!"))
+      .child(element("child").text("Goodbye World!"));
+    const xml = renderToString(root, { version: "1.1" });
 
-  assertEquals(
-    xml,
-    `<?xml version="1.0" encoding="UTF-8"?><root><child>Hello World!</child><child>Goodbye World!</child></root>`,
-  );
+    assertEquals(
+      xml,
+      `<?xml version="1.1" encoding="UTF-8"?><root><child>Hello World!</child><child>Goodbye World!</child></root>`,
+    );
+  });
+
+  await t.step("no children", () => {
+    const root = element("root").child(element("child"));
+    const xml = renderToString(root);
+
+    assertEquals(
+      xml,
+      `<?xml version="1.0" encoding="UTF-8"?><root><child /></root>`,
+    );
+  });
+
+  await t.step("indent", () => {
+    const root = element("root")
+      .child(element("child").text("Hello World!"))
+      .child(element("child").text("Goodbye World!"));
+    const xml = renderToString(root, { indent: 2 });
+
+    assertEquals(
+      xml,
+      `<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <child>Hello World!</child>
+  <child>Goodbye World!</child>
+</root>`,
+    );
+  });
 });
