aaed5c2e66dfaa3a6dc2d06db3c55426568417d7
NISHIZAWA Shuntaro <me@s2n.tech>
2023-12-19 10:17:50 +0900

    ci: Add renew release please workflow

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 import { shun_shobon } from "@shun-shobon/prettier-config";
 
 export default shun_shobon({
-	ignoreFiles: ["**/CHANGELOG.md"],
+	ignoreFiles: ["**/CHANGELOG.md", ".release-please-manifest.json"],
 });
